77320d37ac7f3f67c9fb93f3976ff4ba2ff679aa
Ben Burkert <ben@benburkert.com>
2013-09-24 15:16:33 -0700

    Add the reexec stuff triggered by a USR2 signal.

--- lib/thinicorn/backend.rb
+++ lib/thinicorn/backend.rb
@@ -1,17 +1,16 @@
 class Thinicorn
   module Backend
+    include Thin::Logging
 
     SERVER_FD = 'THINICORN_SERVER_FD'
 
     def initialize(*)
       super
 
-      setup
-    end
-
-    def setup
       @fd    = ENV[SERVER_FD].to_i unless ENV[SERVER_FD].nil?
       @klass = connection_class
+
+      cmd = [$0, *ARGV].join(' ')
 
       trap(:USR2) { reexec }
     end
@@ -19,9 +18,21 @@ class Thinicorn
     def connect
       @fd = create_socket.fileno if @fd.nil?
 
+      ENV[SERVER_FD] = @fd.to_s
+
       EventMachine.attach_server @fd, @klass, &method(:initialize_connection)
 
       @signature = EventMachine.instance_eval{@acceptors.keys.first}
     end
+
+    def reexec
+      log ">> SIGUSR2 received - starting graceful restart"
+
+      cmd = [$0, *ARGV].join(' ')
+      POSIX::Spawn::spawn(cmd)
+
+      stop
+    end
+
   end
 end
--- lib/thinicorn/backends/unix_server.rb
+++ lib/thinicorn/backends/unix_server.rb
@@ -7,7 +7,26 @@ class Thinicorn
     end
 
     def create_socket
-      UNIXServer.open(@host)
+      sock = UNIXServer.open(@socket)
+      sock.close_on_exec = false
+      sock.listen(100)
+
+      sock
     end
+
+    def connect
+      super
+
+      sock    = Socket.for_fd(@fd)
+      address = sock.local_address
+      @socket = address.unix_path
+    end
+
+    def reexec
+      @socket = nil # forget about the socket file so we don't clean it up
+
+      super
+    end
+
   end
 end
